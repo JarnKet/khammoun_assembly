0547ebc6174a11b9fd0f008370923fa7e7dd4a6c
Ketsadaphone BOUTPANYDA <100824073+JarnKet@users.noreply.github.com>
2023-07-09 08:07:42 +0700

    Create Loading Component for better UX

---
+++ src/app/news/loading.js
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <span className="loading loading-ring loading-lg"></span>
+      <h1>ກຳລັງໂຫຼດຂໍ້ມູນ...</h1>
+    </div>
+  );
+};
+
+export default Loading;
