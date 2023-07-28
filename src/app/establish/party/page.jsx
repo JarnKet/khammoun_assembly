e0b31dcb73c3197e6245ddb2118c797934bea350
JarnKet <ketsadaphone999@gmail.com>
2023-07-28 21:15:36 +0700

    change new title

--- src/app/establish/party/page.jsx
+++ src/app/establish/party/page.jsx
@@ -2,13 +2,13 @@ import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export const metadata = {
-  title: "ການຈັດຕັ້ງ",
+  title: "ການຈັດຕັ້ງຂອງສະພາປະຊາຊົນແຂວງຄຳມ່ວນ",
 };
 
 const Page = () => {
   return (
     <>
-      <Banner text={`ການຈັດຕັ້ງ`} />
+      <Banner text={`ການຈັດຕັ້ງຂອງສະພາປະຊາຊົນແຂວງຄຳມ່ວນ`} />
       <section className="flex flex-col items-center justify-center lg:min-h-screen">
         <div className="relative w-[320px] h-[240px] lg:w-[1000px] lg:h-[500px]">
           <Image
